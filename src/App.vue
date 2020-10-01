0fbc4e92ec554843c440d392bb4921469c11eecf
dedoblege <dedoblege@gmail.com>
2020-10-01 08:03:34 +0200

    intro to props

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,16 @@
   <section>
     <header><h1>My Friends</h1></header>
     <ul>
-      <friend-contact></friend-contact>
-      <friend-contact></friend-contact>
+      <friend-contact
+        name="Manuel Lorenz"
+        phone-number="0123 45678 90"
+        email-address="manuel@localhost.com"
+      ></friend-contact>
+      <friend-contact
+        name="Julie Jules"
+        phone-number="0123 45678 90"
+        email-address="juliel@localhost.com"
+      ></friend-contact>
     </ul>
   </section>
 </template>
@@ -11,22 +19,7 @@
 <script>
 export default {
   data() {
-    return {
-      friends: [
-        {
-          id: "manuel",
-          name: "Manuel Lorenz",
-          phone: "0123 45678 90",
-          email: "manuel@localhost.com",
-        },
-        {
-          id: "julie",
-          name: "Julie Jones",
-          phone: "0987 654421 21",
-          email: "julie@localhost.com",
-        },
-      ],
-    }
+    return {}
   },
 }
 </script>
