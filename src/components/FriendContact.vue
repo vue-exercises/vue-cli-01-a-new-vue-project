d40b02655c94dbf561d69c23e32a66795e31f63d
dedoblege <dedoblege@gmail.com>
2020-10-01 08:45:12 +0200

    defining and validating custom events

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -43,6 +43,16 @@ export default {
       // },
     },
   },
+  emits: {
+    "toggle-favorite": function(id) {
+      if (id) {
+        return true
+      } else {
+        console.warn("Id is missing")
+        return false
+      }
+    },
+  },
   data() {
     return {
       detailsAreVisible: false,
